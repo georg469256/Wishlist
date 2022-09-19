b9732be6c03e0f017714a7795d08d681179aa6b9
georg469256 <michael.george20152@gmail.com>
2022-09-19 18:03:27 -0500

    Added input validation/formatting for edit text objects. Submit button is disabled unless all input has been provided.

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.wishlist
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
+import android.webkit.URLUtil
 import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +35,13 @@ class MainActivity : AppCompatActivity() {
             window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
         }
 
+        /**
+         * Checks if given string is a valid url
+         */
+        fun isValidUrl(url: String): Boolean {
+            return URLUtil.isValidUrl(url)
+        }
+
         val itemRV = findViewById<RecyclerView>(R.id.itemsRV)
         val items = ArrayList<Item>()
         val adapter = ItemAdapter(items)
@@ -38,6 +49,65 @@ class MainActivity : AppCompatActivity() {
         val itemURL = findViewById<EditText>(R.id.ItemURL)
         val itemPrice = findViewById<EditText>(R.id.ItemPrice)
         val submitButton = findViewById<Button>(R.id.SubmitButton)
+
+        /**
+         * Returns true if all EditTexts have valid input
+         */
+        fun enableSubmit():Boolean {
+            return (itemName.text.toString() != "" && itemPrice.text.toString() != "" && isValidUrl(itemURL.text.toString()))
+        }
+
+
+        itemName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                submitButton.isEnabled = enableSubmit()
+            }
+
+        })
+
+        itemPrice.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if(itemPrice.text.toString() != "") {
+                    itemPrice.removeTextChangedListener(this)
+                    val cleanString = itemPrice.text.toString().replace("[$,.]".toRegex(),"")
+                    val currencyFormat = DecimalFormat("###,##0.00")
+                    val formatted = "$" + currencyFormat.format(cleanString.toDouble()/100).toString()
+                    itemPrice.setText(formatted)
+                    itemPrice.setSelection(formatted.length)
+                    submitButton.isEnabled = enableSubmit()
+                    itemPrice.addTextChangedListener(this)
+                }
+            }
+        })
+
+        itemURL.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(input: CharSequence?, start: Int, before: Int, count: Int) {
+                if(itemURL.text.toString() != "" && !isValidUrl(itemURL.text.toString())) {
+                    itemURL.error = "URL is not formatted correctly"
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                submitButton.isEnabled = enableSubmit()
+            }
+        })
+
         submitButton.setOnClickListener {
             val newItem = Item(itemName.text.toString(), itemPrice.text.toString(), itemURL.text.toString())
             itemName.setText("")
