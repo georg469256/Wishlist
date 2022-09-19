a7f73f651228a509ba4a16ef652e4196e0cccab4
georg469256 <michael.george20152@gmail.com>
2022-09-19 16:10:02 -0500

    Added ability to enter new item and update recycler view

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.wishlist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,9 +14,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * Parameters / Fields:
+         *   N/A
+         *
+         * Returns nothing:
+         *   closes soft keyboard
+         */
+        fun closeKeyboard() {
+            val view = this.currentFocus
+            if(view != null) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+        }
+
         val itemRV = findViewById<RecyclerView>(R.id.itemsRV)
-        val items = Item.createItemsList(30)
+        val items = ArrayList<Item>()
         val adapter = ItemAdapter(items)
+        val itemName = findViewById<EditText>(R.id.ItemName)
+        val itemURL = findViewById<EditText>(R.id.ItemURL)
+        val itemPrice = findViewById<EditText>(R.id.ItemPrice)
+        val submitButton = findViewById<Button>(R.id.SubmitButton)
+        submitButton.setOnClickListener {
+            val newItem = Item(itemName.text.toString(), itemPrice.text.toString(), itemURL.text.toString())
+            itemName.setText("")
+            itemPrice.setText("")
+            itemURL.setText("")
+            items.add(newItem)
+            adapter.notifyItemInserted(items.size-1)
+            closeKeyboard()
+        }
         itemRV.adapter = adapter
         itemRV.layoutManager = LinearLayoutManager(this)
     }
